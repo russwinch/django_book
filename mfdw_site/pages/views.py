f41814489f44d3625b65534763a3b2e5ed4331d7
Russ Winch <russell.winch@interoute.com>
2018-07-20 16:10:22 +0100

    Start extending the template.

--- mfdw_site/pages/views.py
+++ mfdw_site/pages/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>This is my Homepage</h1>")
-
-# Create your views here.
+    return render(request, 'base.html')
