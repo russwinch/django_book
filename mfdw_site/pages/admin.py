6fd88455755ac1eb5e90ea0e59d84959751c58e6
Russ Winch <russell.winch@interoute.com>
2018-07-20 15:30:20 +0100

    Add page admin. Create static template elements.

--- mfdw_site/pages/admin.py
+++ mfdw_site/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'update_date')
+    ordering = ('title',)
+    search_fields = ('title',)
+
+
+admin.site.register(Page, PageAdmin)
