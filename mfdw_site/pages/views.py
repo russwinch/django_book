efdf49023ce95e8f0055e20e08a3839fe579f669
Russ Winch <russell.winch@interoute.com>
2018-07-25 16:06:06 +0100

    Copyright date is dynamic. Add nav and formatting.

--- mfdw_site/pages/views.py
+++ mfdw_site/pages/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render
 
 from . models import Page
@@ -6,10 +7,13 @@ from . models import Page
 def index(request, pagename):
     pagename = '/' + pagename
     pg = Page.objects.get(permalink=pagename)
+    current_year = date.today().year
     context = {
         'title': pg.title,
         'content': pg.bodytext,
-        'last_updated': pg.update_date
+        'last_updated': pg.update_date,
+        'current_year': current_year,
+        'pages_list': Page.objects.all()
     }
     # assert False
     return render(request, 'pages/page.html', context)
